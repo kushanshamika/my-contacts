bc1cf8f8f1a9afcf92dbb216bb11458ea65944ee
Kushan Shamika <shamikakushan@gmail.com>
2022-01-11 00:44:34 +0530

    Hosting Setup ☁️

--- js/helpers/url_helper.js
+++ js/helpers/url_helper.js
@@ -1,5 +1,5 @@
 $.ajaxPrefilter(function(options, originalOptions, jqXHR) {
-    options.url = 'http://localhost:8888/my-contacts/index.php/api' + options.url
+    options.url = 'https://w1715093.users.ecs.westminster.ac.uk/index.php/api' + options.url
 })
 
 // http://localhost:8888/my-contacts/index.php/api
